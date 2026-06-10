684092e4439fc0999b29f4aaf8d94c9721446571
nekkess <franciszekessler@gmail.com>
2026-06-10 22:29:21 +0200

    fix cloudinary config

--- config/settings.py
+++ config/settings.py
@@ -52,8 +52,15 @@ INSTALLED_APPS = [
 'cloudinary_storage',
 ]
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+
+
+cloudinary.config(
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
+    secure=True
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
